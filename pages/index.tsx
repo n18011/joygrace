b296036b58bde5d4cccdaa0cc002c7b8a4f3da16
n18011 <n18011@std.it-college.ac.jp>
2021-09-05 01:57:30 +0900

    chg static text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Layout from '../components/Layout'
+import { GetStaticProps } from 'next'
 
-const Home: React.FC = () => {
+const Home: React.FC<any> = ({ domainName }) => {
   return (
     <Layout title="Home">
       <p className="text-4xl">Welcome to Next js</p>
-      <p className="text-4xl">{process.env.microcms_domain}</p>
+      <p className="text-4xl">{domainName}</p>
     </Layout>
   )
+}
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      domainName: process.env.microcms_domain,
+    },
+  }
 }
 export default Home
